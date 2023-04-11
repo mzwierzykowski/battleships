7f6bcc45896c5cf605c83b7f39ea476a146b070c
Maciej Zwierzykowski <maciej.zwierzykowski@cshark.com>
2023-04-11 15:14:13 +0200

    Working API

---
+++ Warships.API/Validators/Abstract/IRequestValidator.cs
@@ -0,0 +1,9 @@
+﻿using Warships.API.Models;
+
+namespace Warships.API.Validators.Abstract
+{
+    public interface IRequestValidator
+    {
+        public bool IsValid(Request request);
+    }
+}
---
+++ Warships.Configuration/ExceptionMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Warships.Setup.Models
+{
+    public static class ExceptionMessages
+    {
+        public static readonly string MissingFleetConfiguration = "Unable to create fleet: missing fleet configuration.";
+        public static readonly string MissingBoardDimensionConfiguration = "Unable to generate board: missing board dimension configuration.";
+        public static readonly string BuildShipCircuitBreakerException = "Exceeded maximum attemtps limit on building ship";
+    }
+}

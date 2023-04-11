ba0b9b421b290ed9eb151e96cf3ef34ce50fa8ba
Maciej Zwierzykowski <maciej.zwierzykowski@cshark.com>
2023-04-11 15:13:40 +0200

    Working WebAPI

--- Warships.API/Program.cs
+++ Warships.API/Program.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Warships.API.Models;
 using Warships.API.Models.Mapping;
+using Warships.API.Validators;
+using Warships.API.Validators.Abstract;
 using Warships.Configuration;
 using Warships.Game.DI;
 using Warships.Game.Services.Abstract;
@@ -9,20 +11,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<BoardDimension>(builder.Configuration.GetSection("BoardDimension"));
 builder.Services.Configure<FleetConfiguration>(builder.Configuration.GetSection("FleetConfiguration"));
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddSingleton<IRequestValidator, RequestValidator>();
 builder.Services.AddGameServices();
 
 var app = builder.Build();
 
-app.MapGet("/game", (IMapper mapper, IGameService gameService) => {
-    var gameState = gameService.StartGame();
-    var result = mapper.Map<GameState>(gameState);
-    return TypedResults.Ok(result);
+app.MapGet("/game", (IGameService gameService) => {
+    var result = gameService.StartGame();
+    return result;
 });
 
-app.MapPost("/game",  (string pointId, IMapper mapper, IGameService gameService) => {
-    var gameState = gameService.ShootsFired(pointId);
-    var result = mapper.Map<GameState>(gameState);
-    return TypedResults.Ok(result);
+app.MapPost("/game", (Request request, IRequestValidator requestValidator, IMapper mapper, IGameService gameService) => {
+    if (requestValidator.IsValid(request))
+    {
+        var gameState = gameService.ShootsFired(request.PointId);
+        var result = mapper.Map<GameState>(gameState);
+        return Results.Ok(result);
+    }
+    else
+    {
+        return Results.BadRequest("Requested point was not in a correct format");
+    }
 });
 
 app.Run();
